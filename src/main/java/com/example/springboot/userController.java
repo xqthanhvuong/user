74904c5885a0c621c23c17b85786377850f2ca13
xqthanhvuong <hoangtuquy775@gmail.com>
2025-06-28 11:05:54 +0700

    first commit

---
+++ src/main/java/com/example/springboot/userController.java
@@ -0,0 +1,35 @@
+package com.example.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class userController {
+
+	@GetMapping("/")
+	public String index() {
+		return "User service!";
+	}
+
+    @GetMapping("/user")
+    public User getUser() {
+        return new User("Bod");
+    }
+}
+
+class User {
+	private String name;
+
+	public User(String name) {
+		this.name = name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
